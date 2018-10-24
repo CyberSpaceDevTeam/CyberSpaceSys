e76830fb52fc004272d2bfb9d1a7401eccaa2a6d
Justdwiwt <len_master@hotmail.com>
2018-10-24 20:29:35 +0800

    add common & utils config

---
+++ cyber-common/src/main/java/com/cyber/common/utils/ObjectUtils.java
@@ -0,0 +1,14 @@
+package com.cyber.common.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * static mapper
+ *
+ * @author Justdwiwt
+ */
+public class ObjectUtils {
+
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+}
