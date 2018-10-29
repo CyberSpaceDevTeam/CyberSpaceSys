c1cedc8a170472d6d9ba925bff08ebac0c8af677
Justdwiwt <len_master@hotmail.com>
2018-10-29 21:58:59 +0800

    update maven config

---
+++ cyber-manager/cyber-manager-web/src/main/java/com/cyber/web/controller/Starter.java
@@ -0,0 +1,22 @@
+package com.cyber.web.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@SpringBootApplication
+@Controller
+public class Starter {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Starter.class, args);
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String test() {
+        return "index";
+    }
+
+}
